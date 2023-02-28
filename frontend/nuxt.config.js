b3b5e08853d282fece00e20e1523a77c67dd157b
ivan <ivan@positron-it.ru>
2023-02-28 20:35:48 +0300

    Some config changes

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -27,37 +27,51 @@ export default {
 
   target: 'server',
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/scss/global.scss'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  styleResources: {
+    scss: ['~/assets/scss/*.scss']
+  },
+
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/'
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    extractCSS: true,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css)$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
+    }
+  },
+
+  robots: [
+    {
+      UserAgent: '*',
+      Disallow: '/'
+    }
+  ]
 }
